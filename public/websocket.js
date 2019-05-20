a78eddf8eb3df564653ed6f37a6667dbadd3880e
Liam Hilliard <liam.h.-@hotmail.com>
2019-05-20 02:38:13 +0100

    Remove Firebase
    In order to implement websockets myself, I need to move from firebase to Heroku for hosting

--- public/websocket.js
+++ public/websocket.js
@@ -1,7 +1,7 @@
 (function(){
 	"use strict";
 
-	const ws = new WebSocket('ws://localhost:5001');
+	const ws = new WebSocket('ws://localhost:5001/videoshare-68389/us-central1/app');
 
 	ws.addEventListener('open', function(event){
 		console.log('ws: open', event);
---
+++ server.js
@@ -0,0 +1,25 @@
+const conf = require('../conf.js');
+const express = require('express');
+
+const app = express();
+console.log('------- STARTING APP -------');
+
+app.use(function(request, response, next){
+	console.log('--> new request');
+	console.log('request.socket', request.socket)
+	console.log('request.httpVersion', request.httpVersion);
+	console.log('request.headers', request.headers);
+	console.log('request.upgrade', request.upgrade);
+	console.log('request.url', request.url);
+	console.log('request.method', request.method);
+	console.log('request.statusCode', request.statusCode);
+
+	return next();
+});
+
+app.get('/', (request, response) => {
+	response.sendFile('index.html', { root: conf.ROOT });
+});
+
+app.use(express.static(conf.ROOT));
+
