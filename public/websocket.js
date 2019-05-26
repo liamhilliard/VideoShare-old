c38d0b621487994dded53746bdc767d83a5a6203
Liam Hilliard <liam.h.-@hotmail.com>
2019-05-26 02:54:55 +0100

    Change URI for WS connection

--- public/websocket.js
+++ public/websocket.js
@@ -1,13 +1,12 @@
 (function(){
 	"use strict";
 
-	const ws = new WebSocket('ws://'+ window.location.hostname +'/');
+	const ws = new WebSocket('ws://'+ window.location.host +'/');
 
 	ws.addEventListener('open', function(event){
 		console.log('ws: open', event);
 		ws.send('Hello, server world!');
 	});
-
 
 	ws.addEventListener('message', function(event){
 		console.log('ws: message', event);
