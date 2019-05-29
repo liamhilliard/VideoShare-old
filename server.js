2a5be6bd918cbe4773b6c62ef79f5930d246b339
Liam Hilliard <liam.h.-@hotmail.com>
2019-05-29 23:38:29 +0100

    Add test event handlers to ws

--- server.js
+++ server.js
@@ -4,7 +4,25 @@ const coreController = require('./routes/core.controller');
 const app = express();
 const port = process.env.PORT || 5001;
 
-app.use('/', coreController(app));
+// app.use('/', coreController(app));
+const WebSocket = require('ws');
+const WebSocketServer = new WebSocket.Server({ server: app, path: '/wsconn' });
+
+WebSocketServer.on('connection', function(socket){
+	console.log('### connection established');
+
+	socket.on('close', (a) => { console.log('### close', a); });
+	socket.on('error', (a) => { console.log('### error', a); });
+	socket.on('open', (a) => { console.log('### open', a); });
+	socket.on('ping', (a) => { console.log('### ping', a); });
+	socket.on('pong', (a) => { console.log('### pong', a); });
+	socket.on('unexpected-response', (a) => { console.log('### unexpected-response', a); });
+	socket.on('upgrade', (a) => { console.log('### upgrade', a); });
+	socket.on('message', (a) => { console.log('### message', a); });
+
+	socket.send('Hello, WS world!');
+});
+
 
 app.listen(port, () => {
 	console.log('### App listening on port '+ port);
