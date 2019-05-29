d318d600e3b00c23b5e206006d81f78e3f883854
Liam Hilliard <liam.h.-@hotmail.com>
2019-05-29 23:52:23 +0100

    Match code from ws npm example

--- routes/core.controller.js
+++ routes/core.controller.js
@@ -4,20 +4,33 @@ const conf = require('../conf');
 const coreRouter = express.Router();
 
 module.exports = function(app){
-    // // Set up WebSockets
-    // const WebSocket = require('ws');
-    // const WebSocketServer = new WebSocket.Server({ server: app, path: '/wsconn' });
+    // Set up WebSockets
+    const WebSocket = require('ws');
+    const WebSocketServer = new WebSocket.Server({ server: app, path: '/wsconn' });
 
-    // WebSocketServer.on('connection', function(socket){
-    //     console.log('connection established');
+    WebSocketServer.on('connection', function(socket){
+        console.log('### connection established');
+        
+        socket.on('close', (a) => { console.log('### close', a); });
+        socket.on('error', (a) => { console.log('### error', a); });
+        socket.on('open', (a) => { console.log('### open', a); });
+        socket.on('ping', (a) => { console.log('### ping', a); });
+        socket.on('pong', (a) => { console.log('### pong', a); });
+        socket.on('unexpected-response', (a) => { console.log('### unexpected-response', a); });
+        socket.on('upgrade', (a) => { console.log('### upgrade', a); });
+        socket.on('message', (a) => { console.log('### message', a); });
+        
+        socket.send('Hello, WS world!');
+    });
 
-    //     socket.on('message', function(message){
-    //         console.log('message', message);
-    //     });
-
-    //     socket.send('Hello, WS world!');
-    // });
-
+    app.on('upgrade', function(request, socket, head){
+        console.log('### upgrade event');
+        WebSocketServer.handleUpgrade(request, socket, head, (ws) => {
+            console.log('### handleUpgrade done');
+            WebSocketServer.emit('connection', ws, request);
+        });
+    });
+    
     // Serve static files
     coreRouter.use(express.static(conf.ROOT));   
     return coreRouter; 
